9056b650c60bf61238585eef6b243bd96f8e7d40
Diana.Melnychuk <nikxy3@gmail.com>
2024-02-26 17:47:37 +0200

    remove vpc

--- main.tf
+++ main.tf
@@ -18,7 +18,6 @@ locals {
 resource "aws_security_group" "my_sg" {
   name        = "allow-myssh-allhttp-s"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = "vpc-08d0690f631a02c74"
   egress {
     from_port        = 0
     to_port          = 0
